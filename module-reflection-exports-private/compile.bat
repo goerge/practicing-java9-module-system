bd42e8fdc87503331b41ae82172b6555e022a8f4
Peter Kofler <peter.kofler@code-cop.org>
2019-09-23 11:59:45 +0200

    Regular compile/run scripts for reflection.

---
+++ module-reflection-exports-private/compile.bat
@@ -0,0 +1,3 @@
+javac printer-impl\src\module-info.java printer-impl\src\com\printer\impl\Printer.java -d dist\printer-impl
+
+javac printer-client\src\module-info.java printer-client\src\com\printer\client\PrinterClient.java -p dist\printer-impl -d dist\printer-client
